5ba8ca5ab0d4b4bd0fe9a2bd4cb143a4133c70db
Carlos <carlos.araya@gmal.com>
2014-03-23 16:48:50 -0700

    Cleaned directory

--- rayuela/utilities.js
+++
@@ -1,9 +0,0 @@
-/**
-Creates a random dataset for experimental purposes
-*/
-var dataset = [];                                                                           //Initialize empty array
-for (var i = 0; i < 25; i++) {                                                        //Loop 25 times, change the number to change the size of the array
-    var newNumber = Math.floor(Math.random() * 30 +1);     //New random integer  (1-30)
-    dataset.push(newNumber);                                                    //Add new number to array
-}
-
